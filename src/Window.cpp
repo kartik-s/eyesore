32f5deaff585de9ba3f84563a770c974781bff68
Kartik Singh <kartiksingh_ma@yahoo.com>
2017-04-10 17:55:50 -0400

    Add Window class

---
+++ src/Window.cpp
@@ -0,0 +1,20 @@
+#include "Window.h"
+
+using std::string;
+
+eyesore::Window::Window(int width, int height, string title, bool fullscreen)
+{
+	window = glfwCreateWindow(width, height, title.c_str(),
+			fullscreen ? glfwGetPrimaryMonitor() : nullptr, nullptr);
+}
+
+eyesore::Window::~Window()
+{
+	glfwDestroyWindow(window);
+}
+
+GLFWwindow *eyesore::Window::get()
+{
+	return window;
+}
+
---
+++ src/Window.h
@@ -0,0 +1,22 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <string>
+
+#include <GLFW/glfw3.h>
+
+namespace eyesore {
+	class Window {
+		public:
+			Window(int width, int height, std::string title, bool fullscreen);
+			~Window();
+
+			GLFWwindow *get();
+
+		private:
+			GLFWwindow *window;
+	};
+}
+
+#endif
+
