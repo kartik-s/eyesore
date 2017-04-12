c0c91ad4d18f01ce5b46d9c62945851388684c1e
Kartik Singh <kartiksingh_ma@yahoo.com>
2017-04-11 22:08:49 -0400

    Make Vertex properties public

--- src/Vertex.h
+++ src/Vertex.h
@@ -6,11 +6,10 @@
 namespace eyesore {
 	class Vertex {
 		public:
-			Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 uv);
-
-		private:
 			glm::vec3 position, normal;
 			glm::vec2 uv;
+
+			Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 uv);
 	};
 }
 
