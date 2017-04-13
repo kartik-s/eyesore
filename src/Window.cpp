fb16c3f3c574844bcff0cb768528b8bd66744246
Kartik Singh <kartiksingh_ma@yahoo.com>
2017-04-12 23:24:17 -0400

    Add window hints

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,9 +1,14 @@
+#include <GL/glew.h>
+
 #include "Window.h"
 
 using std::string;
 
 eyesore::Window::Window(int width, int height, string &title, bool fullscreen)
 {
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	window = glfwCreateWindow(width, height, title.c_str(),
 			fullscreen ? glfwGetPrimaryMonitor() : nullptr, nullptr);
 }
