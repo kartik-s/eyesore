08edb05921b8d4fe7f6725264a0800719a41e163
Kartik Singh <kartiksingh_ma@yahoo.com>
2017-04-13 09:57:52 -0400

    Add Camera to Scene

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -2,6 +2,11 @@
 
 using namespace eyesore;
 
+eyesore::Scene::Scene(Camera camera): camera(camera)
+{
+
+}
+
 void eyesore::Scene::add(Model model)
 {
 	models.push_back(model);
--- src/Scene.h
+++ src/Scene.h
@@ -4,15 +4,18 @@
 #include <vector>
 
 #include "Model.h"
+#include "Camera.h"
 
 namespace eyesore {
 	class Scene {
 		public:
+			Scene(eysore::Camera camera);
 			void add(eyesore::Model model);
 			void render() const;
 
 		private:
 			std::vector<eyesore::Model> models;
+			eyesore::Camera camera;
 	};
 }
 
