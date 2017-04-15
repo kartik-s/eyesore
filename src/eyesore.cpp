f103a40cbffc6491a966ae1af3a9b5d253c8cd52
Kartik Singh <kartiksingh_ma@yahoo.com>
2017-04-15 11:16:31 -0400

    Test everything in driver

--- src/eyesore.cpp
+++ src/eyesore.cpp
@@ -1,12 +1,21 @@
+#include <glm/glm.hpp>
+
 #include "eyesore.h"
+#include "Scene.h"
+#include "Model.h"
 
 using namespace eyesore;
+using namespace glm;
 
 int main(void)
 {
-	Engine e;
+	Camera c(vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 0.0f, -1.0f), 4.0f/3.0f);
+	Engine e(c);
+	Scene s;
 
 	e.makeWindow(400, 300, "eyesore", false);
+	s.add(Model("../models/teapot.obj"));
+	e.setScene(s);
 	e.run();
 
 	return 0;
