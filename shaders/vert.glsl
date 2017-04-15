d3844fc5a3270bcd6588a37b306aa23e169d4c22
Kartik Singh <kartiksingh_ma@yahoo.com>
2017-04-15 11:15:36 -0400

    Use view and projection matrices in vertex shader

--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -2,8 +2,11 @@
 
 layout (location = 0) in vec3 position;
 
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-	gl_Position = vec4(position.x, position.y, position.z, 1.0);
+	gl_Position = projection * view * vec4(position, 1.0);
 }
 
