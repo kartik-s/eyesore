f0a922696d3dd37c98c5f3168839de768508b021
Kartik Singh <kartiksingh_ma@yahoo.com>
2017-04-15 11:11:10 -0400

    Add Camera view and projection matrix functionality

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,12 +1,37 @@
+#include <iostream>
+
+#include <GL/glew.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Camera.h"
 
-using namespace eyespre;
-using namespace glm;
+using namespace eyesore;
 
-eyesore::Camera::Camera(vec3 position, vec3 direction):
-	position(position), direction(normalize(direction))
+using glm::vec3;
+using glm::mat4;
+
+eyesore::Camera::Camera(vec3 position, vec3 direction, GLfloat aspectRatio,
+		GLfloat fov):
+	position(position), direction(normalize(direction)), aspectRatio(aspectRatio),
+	fov(fov)
 {
 	right = normalize(cross(direction, vec3(0, 1, 0)));
 	up = normalize(cross(right, direction));
+}
+
+void eyesore::Camera::use() const
+{
+	GLint program;
+
+	glGetIntegerv(GL_CURRENT_PROGRAM, &program);
+
+	mat4 view = glm::lookAt(position, position + direction, up);
+	mat4 projection = glm::perspective(fov, aspectRatio, near, far);
+	GLint viewLoc = glGetUniformLocation(program, "view");
+	GLint projectionLoc = glGetUniformLocation(program, "projection");
+
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 }
 
--- src/Camera.h
+++ src/Camera.h
@@ -3,13 +3,20 @@
 
 #include <glm/glm.hpp>
 
+#include "ShaderProgram.h"
+
 namespace eyesore {
 	class Camera {
 		public:
-			Camera(glm::vec3 position, glm::vec3 direction);
-			
+			Camera(glm::vec3 position, glm::vec3 direction, GLfloat aspectRatio,
+					GLfloat fov = 45.0f);
+
+			void use() const;
+
 		private:
 			glm::vec3 position, direction, right, up;
+			GLfloat aspectRatio, fov;
+			GLfloat near = 0.1f, far = 1000.0f;
 	};
 }
 
