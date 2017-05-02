dd81661f75918ee6c024fdf432c257de6ac2bb88
Kartik Singh <kartiksingh_ma@yahoo.com>
2017-05-02 09:22:54 -0400

    Enable anti-aliasing

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -4,15 +4,14 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 #include "Engine.h"
 #include "Window.h"
+#include "KeyboardHandler.h"
 
 using namespace eyesore;
 
 using std::cerr;
-using std::endl;
-using std::string;
+using std::endl; using std::string;
 using std::exit;
 
 void errorHandler(int error, const char *description)
@@ -23,7 +22,7 @@ void errorHandler(int error, const char *description)
 	exit(EXIT_FAILURE);
 }
 
-eyesore::Engine::Engine(Camera camera): camera(camera)
+eyesore::Engine::Engine(Camera *camera): camera(camera), input(nullptr, camera)
 {
 	glfwSetErrorCallback(errorHandler);
 	glfwInit();
@@ -49,6 +48,8 @@ void eyesore::Engine::makeWindow(int width, int height, string title, bool fulls
 
 	glfwGetFramebufferSize(window->get(), &w, &h);
 	glViewport(0, 0, w, h);
+
+	input.setWindow(window);
 }
 
 void eyesore::Engine::setScene(Scene scene)
@@ -58,10 +59,15 @@ void eyesore::Engine::setScene(Scene scene)
 
 void eyesore::Engine::run()
 {
-	while (!glfwWindowShouldClose(window->get())) {
-		glClear(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_MULTISAMPLE);
 
-		scene.render(camera);
+	while (!glfwWindowShouldClose(window->get())) {
+		glClearColor(0.95f, 0.95f, 0.95f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		input.dispatch();
+		scene.render(*camera);
 
 		glfwSwapBuffers(window->get());
 		glfwPollEvents();
--- src/Window.cpp
+++ src/Window.cpp
@@ -9,6 +9,7 @@ eyesore::Window::Window(int width, int height, string &title, bool fullscreen)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
 	window = glfwCreateWindow(width, height, title.c_str(),
 			fullscreen ? glfwGetPrimaryMonitor() : nullptr, nullptr);
 }
